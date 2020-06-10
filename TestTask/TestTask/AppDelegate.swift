4a822eb2b67014cf771d87c68e26a67396f8fe49
Alexander Kraynov <kraynovsasha@gmail.com>
2020-06-11 02:28:13 +0300

    Updated annotations

--- TestTask/TestTask/AppDelegate.swift
+++ TestTask/TestTask/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  TestTask
-//
-//  Created by Александр Крайнов on 09.06.2020.
-//  Copyright © 2020 Александр Крайнов. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
--- TestTask/TestTask/SceneDelegate.swift
+++ TestTask/TestTask/SceneDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.swift
-//  TestTask
-//
-//  Created by Александр Крайнов on 09.06.2020.
-//  Copyright © 2020 Александр Крайнов. All rights reserved.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
